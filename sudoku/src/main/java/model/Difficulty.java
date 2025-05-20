b0c11a4e84a1815dad4a08e6ddb8c3d6e9863291
Manuel Buizo <buizomanuel@gmail.com>
2025-05-20 16:46:46 +0200

    feat: grid data

---
+++ sudoku/src/main/java/model/Difficulty.java
@@ -0,0 +1,17 @@
+package model;
+
+public enum Difficulty {
+    EASY(30),
+    MEDIUM(40),
+    HARD(50);
+
+    private final int percent;
+
+    Difficulty(final int percent) {
+        this.percent = percent;
+    }
+
+    public int percent() {
+        return this.percent;
+    }
+}
