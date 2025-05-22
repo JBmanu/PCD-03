1d261ab80f92e77edd72f7a5f636aa3297b83c37
Manuel Buizo <buizomanuel@gmail.com>
2025-05-22 10:19:26 +0200

    feat: OptionsListener

---
+++ sudoku/src/main/java/view/listener/OptionsListener.java
@@ -0,0 +1,16 @@
+package view.listener;
+
+public interface OptionsListener {
+    
+    void onExit();
+    
+    void onSuggest();
+    
+    void onUndo();
+    
+    void onReset();
+    
+    void onLightMode();
+    
+    void onDarkMode();
+}
