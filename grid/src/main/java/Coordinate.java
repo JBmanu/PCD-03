aa9288ba2b4896a4167aa92718c10059176f17c3
Manuel Buizo <buizomanuel@gmail.com>
2025-06-04 14:22:11 +0200

    feat: create grid and test

---
+++ grid/src/main/java/Coordinate.java
@@ -0,0 +1,20 @@
+public interface Coordinate {
+
+    static Coordinate create(final int row, final int column) {
+        return new CoordinateImpl(row, column);
+    }
+    
+
+    int row();
+
+    int column();
+    
+
+    record CoordinateImpl(int row, int column) implements Coordinate {
+
+        @Override
+        public String toString() {
+            return "POS: row[" + this.row + "] column[" + this.column + "]";
+        }
+    }
+}
