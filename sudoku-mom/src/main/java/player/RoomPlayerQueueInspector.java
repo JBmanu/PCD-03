9d2b764502ddf559607c14b051eaf6dcb039643b
Manuel Buizo <buizomanuel@gmail.com>
2025-06-05 11:29:39 +0200

    feat: create inspector to the query on cloudAMQP

--- sudoku-mom/src/main/java/player/RoomPlayerQueueInspector.java
+++ sudoku-mom/src/main/java/player/RoomPlayerQueueInspector.java
@@ -1,6 +1,5 @@
 package player;
 
-//other name: GameRoomQueueDiscovery
 
 import com.rabbitmq.http.client.Client;
 import com.rabbitmq.http.client.ClientParameters;
@@ -9,42 +8,57 @@ import com.rabbitmq.http.client.domain.ConnectionInfo;
 import com.rabbitmq.http.client.domain.ExchangeInfo;
 import com.rabbitmq.http.client.domain.QueueInfo;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+//other name: GameRoomQueueDiscovery
 public class RoomPlayerQueueInspector {
+    private static final String HOST = "kangaroo.rmq.cloudamqp.com";
+    private static final String USERNAME = "fanltles";
+    private static final String PASSWORD = "6qCOcwZEWGpkuiJnzfvybUUeXfHy1oM0";
+    private static final String URL = "https://" + HOST + "/api/";
 
-    public static void main(final String[] args) throws Exception {
-        final String host = "kangaroo.rmq.cloudamqp.com";
-        final String username = "fanltles";
-        final String password = "6qCOcwZEWGpkuiJnzfvybUUeXfHy1oM0";
-        
-        final Client client = new Client(new ClientParameters()
-                .url("https://" + host + "/api/")
-                .username(username)
-                .password(password));
+    private final Client client;
 
-        // Ottenere le code
-        final List<QueueInfo> queues = client.getQueues();
+    public RoomPlayerQueueInspector() {
+        try {
+            this.client = new Client(new ClientParameters()
+                    .url(URL)
+                    .username(USERNAME)
+                    .password(PASSWORD));
+        } catch (final URISyntaxException | MalformedURLException e) {
+            System.out.println("Failed to create RabbitMQ HTTP client: " + e.getMessage());
+            throw new RuntimeException("Failed to create RabbitMQ HTTP client", e);
+        }
+    }
+
+    private void queues() {
+        final List<QueueInfo> queues = this.client.getQueues();
         for (final QueueInfo queue : queues) {
             System.out.println("Coda: " + queue.getName() + ", Messaggi: " + queue.getMessageStats().toString());
         }
+    }
 
-        // Ottenere gli exchange
-        final List<ExchangeInfo> exchanges = client.getExchanges();
+    private void exchanges() {
+        final List<ExchangeInfo> exchanges = this.client.getExchanges();
         for (final ExchangeInfo exchange : exchanges) {
             System.out.println("Exchange: " + exchange.getName() + ", Tipo: " + exchange.getType());
         }
+    }
 
-        // Ottenere le connessioni
-        final List<ConnectionInfo> connections = client.getConnections();
+    private void connections() {
+        final List<ConnectionInfo> connections = this.client.getConnections();
         for (final ConnectionInfo connection : connections) {
             System.out.println("Connessione: " + connection.getName());
         }
+    }
 
-        // Ottenere i canali
-        final List<ChannelInfo> channels = client.getChannels();
+    private void channels() {
+        final List<ChannelInfo> channels = this.client.getChannels();
         for (final ChannelInfo channel : channels) {
             System.out.println("Canale: " + channel.getName());
         }
     }
+
 }
