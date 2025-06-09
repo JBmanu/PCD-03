cb28e3cad196a53520a1c1bd43ec5dce247fd6e8
Manuel Buizo <buizomanuel@gmail.com>
2025-06-09 10:30:28 +0200

    feat: createGrid for server

--- sudoku-mom/src/main/java/model/GridServer.java
+++ sudoku-mom/src/main/java/model/GridServer.java
@@ -12,26 +12,31 @@ public interface GridServer {
 
     void createGrid(Settings settings);
 
-    byte[][] solution();
+    Optional<byte[][]> solution();
+
+    Optional<byte[][]> grid();
 
 
     class GridServerImpl implements GridServer {
         private Optional<Grid> grid;
-        
+
         public GridServerImpl() {
             this.grid = Optional.empty();
         }
-        
+
         @Override
         public void createGrid(final Settings settings) {
             this.grid = Optional.of(Grid.create(settings));
         }
 
         @Override
-        public byte[][] solution() {
-            return this.grid.map(Grid::solution)
-                            .orElseThrow(() -> new IllegalStateException("Grid not created yet"))
-                            .get();
+        public Optional<byte[][]> solution() {
+            return this.grid.map(Grid::solutionArray);
+        }
+
+        @Override
+        public Optional<byte[][]> grid() {
+            return this.grid.map(Grid::cellsArray);
         }
     }
 }
--- sudoku-mom/src/test/java/GridServerTest.java
+++ sudoku-mom/src/test/java/GridServerTest.java
@@ -1,10 +1,11 @@
 import grid.Settings;
 import model.GridServer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GridServerTest {
     private static final List<Settings> SETTINGS = List.of(
@@ -12,20 +13,21 @@ public class GridServerTest {
             Settings.create(Settings.Schema.SCHEMA_9x9, Settings.Difficulty.EASY),
             Settings.create(Settings.Schema.SCHEMA_16X16, Settings.Difficulty.EASY),
             Settings.create(Settings.Schema.SCHEMA_25X25, Settings.Difficulty.EASY));
-    
+
     private GridServer server;
 
-    @Test
+    @BeforeEach
     public void create() {
         this.server = GridServer.create();
     }
-    
+
     @Test
     public void createGrid() {
         SETTINGS.forEach(settings -> {
-           this.server.createGrid(settings);
-           assertNotNull(this.server.solution());
-       })
+            this.server.createGrid(settings);
+            assertTrue(this.server.solution().isPresent());
+            assertTrue(this.server.grid().isPresent());
+        });
     }
-    
+
 }
