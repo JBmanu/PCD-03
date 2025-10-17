50c1b33dc8fdc19fb5a0e3a7aea7336860e98cdb
Manuel Buizo <buizomanuel@gmail.com>
2025-10-17 16:27:44 +0200

    feat: create Stepper

--- simulation-car/src/main/scala/model/inspector/Stepper.scala
+++ simulation-car/src/main/scala/model/inspector/Stepper.scala
@@ -1,9 +1,31 @@
 package model.inspector
 
-trait Stepper
+trait Stepper:
+  def totalStep: Int
+
+  def currentStep: Int
+
+  def totalStep_=(totalStep: Int): Unit
+
+  def increaseStep(): Unit
+
+  def hasMoreSteps(): Boolean
+
 
 object Stepper:
-  
+
   def apply(): Stepper = StepperImpl()
 
-  private case class StepperImpl() extends Stepper
+  private case class StepperImpl() extends Stepper:
+    private var _totalStep: Int = 0
+    private var _currentSteps: Int = 0
+
+    override def totalStep: Int = _totalStep
+
+    override def currentStep: Int = _currentSteps
+
+    override def totalStep_=(totalStep: Int): Unit = _totalStep = totalStep
+
+    override def increaseStep(): Unit = _currentSteps += 1
+
+    override def hasMoreSteps(): Boolean = currentStep < totalStep
