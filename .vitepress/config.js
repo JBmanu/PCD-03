76d13e1c997a7e4c7c14c74219d8a143a51cf43f
Manuel Buizo <buizomanuel@gmail.com>
2026-06-04 19:24:14 +0200

    feat: create vitepress

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,17 +1,19 @@
 import {defineConfig} from 'vitepress'
 import {withMermaid} from "vitepress-plugin-mermaid";
 
-let reportPath = '/report'
-let scopingPath = reportPath + '/scoping'
-let planningPath = reportPath + '/planning'
+let reportSimulationCar = './part1-simulation-car/doc'
+let reportSudokuMOM = './part2A-sudoku-mom/doc'
+let reportSudokuJavaRMI = './part2B-sudoku-rmi/doc'
+let reportGuessNumber = './part3-guess-number/doc'
+
 // https://vitepress.dev/reference/site-config
 
 
 export default withMermaid(
     defineConfig({
-        base: '/AthenaPlay-PM/',
-        title: "AthenaPlay-PM",
-        description: "Ulisse an Train Infrastructure Similator",
+        base: '/PCD-03/',
+        title: "PCD-03",
+        description: "Programmazione Concorrente e Distribuita",
         themeConfig: {
             // https://vitepress.dev/reference/default-theme-config
             nav: [
@@ -22,50 +24,16 @@ export default withMermaid(
                 {
                     text: 'Report',
                     items: [
-                        {text: 'Contesto', link: `${reportPath}/0-contesto`},
-                        {text: 'Scoping', link: `${reportPath}/1-scoping`},
-                        {text: 'Planning', link: `${reportPath}/2-planning`},
-                        {text: 'Launching/Execution', link: `${reportPath}/3-launching-execution`},
-                        {text: 'Monitoring E controlling', link: `${reportPath}/4-monitoring-E-controlling`},
-                        {text: 'Closing', link: `${reportPath}/5-closing`},
+                        {text: 'Contesto', link: `${reportSimulationCar}/report`},
+                        {text: 'Scoping', link: `${reportSudokuMOM}/report`},
+                        {text: 'Planning', link: `${reportSudokuJavaRMI}/report`},
+                        {text: 'Launching/Execution', link: `${reportGuessNumber}/report`},
                     ]
-                },
-                {
-                    text: 'Allegati',
-                    items: [
-                        {
-                            text: 'Scoping',
-                            collapsed: true,
-                            items: [
-                                {text: '1-Meeting', link: `${scopingPath}/1-meeting`},
-                                {text: '2-Meeting', link: `${scopingPath}/2-meeting`},
-                                {text: '3-Meeting', link: `${scopingPath}/3-meeting`},
-                                {text: 'Market Analysis', link: `${scopingPath}/Market-Analysis`},
-                                {text: 'POS', link: `${scopingPath}/POS`},
-                                {text: 'Risk Analysis', link: `${scopingPath}/Risk-Analysis`},
-                                {text: 'Feasibility studies', link: `${scopingPath}/Feasibility-Studies`},
-                                {text: 'RBS', link: `${scopingPath}/RBS`},
-                                {text: 'SWOT Analysis', link: `${scopingPath}/SWOT-Analysis`},
-                            ]
-                        },
-                        {
-                            text: 'Planning',
-                            collapsed: true,
-                            items: [
-                                {text: 'WBS', link: `${planningPath}/WBS`},
-                                {text: 'PND', link: `${planningPath}/PND`},
-                                {text: 'Gantt', link: `${planningPath}/Gantt`},
-                                {text: 'RBS', link: `${planningPath}/RBS`},
-                                {text: 'Risk Management', link: `${planningPath}/Risk-Management-Plan`},
-                            ]
-                        }
-                    ]
-
                 }
             ],
 
             socialLinks: [
-                {icon: 'github', link: 'https://github.com/JBmanu/AthenaPlay-PM'}
+                {icon: 'github', link: 'https://github.com/JBmanu/PCD-03'}
             ]
         }
     })
